6e32cfcfdcf08cf10871e3a7fbfca811b3f13028
ReNN <vladyko.anton@mail.ru>
2025-10-27 17:51:35 +0300

    full dif proc test

--- lab2_regress.py
+++ lab2_regress.py
@@ -17,11 +17,26 @@ from sklearn.metrics import mean_squared_error, mean_absolute_error
 #  MAE: 10.6775
 #Тестовая выборка:
 #  MSE: 196.2622
-#  RMSE: 14.0094
+#  RMSE: 14.0094 - x
 #  MAE: 10.9933
 
+#Обучающая выборка:
+#  MSE: 186.3516
+#  MAE: 10.6657
+#Тестовая выборка:
+#  MSE: 196.1819
+#  RMSE: 14.0065
+#  MAE: 11.0474
+
+#0.2%
+
 pd.set_option('display.max_columns', None)
-df = pd.read_csv("./data/processed_train2.csv")
+df = pd.read_csv("./data/processed_train.csv")
+df2 = pd.read_csv("./data/processed_train2.csv")
+
+df['Age'] = df2['Age']
+
+df = df.drop(['Cabin', 'Name'], axis = 1)
 
 X = df.drop('Age', axis=1)
 y = df['Age']
